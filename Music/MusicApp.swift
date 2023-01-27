eb997764ffe85220e219d37616cd407bd58da33c
Lakhan Lothiyi <llsc12.dev@gmail.com>
2023-01-27 15:46:10 +0000

    Initial Commit

---
+++ Music/MusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicApp.swift
+//  Music
+//
+//  Created by Lakhan Lothiyi on 27/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
